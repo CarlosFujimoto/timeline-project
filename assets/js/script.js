f11fda3aedad9e08683329e656da5c99d26dc953
Carlos Fujimoto <carlos_k.fujimoto@outlook.com>
2024-03-12 19:21:25 -0300

    dark mode implemented on materials page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,64 +2,49 @@ const chk = document.getElementById("chk");
 const header = document.querySelector(".header");
 const label = document.querySelector(".label");
 const navList = document.querySelector(".nav-list");
-const links = document.querySelector(".link-margin-control")
+const links = document.querySelector(".link-margin-control");
 
 function checkDarkMode() {
-    if (localStorage.getItem("darkMode") === "true") {
-      document.body.classList.add("dark");
-      header.classList.add("dark-mode");
-      label.classList.add("dark-mode");
-      navList.classList.add("dark-mode");
-      
-      document.querySelectorAll('.nav-list a').forEach((button) => {
-          button.classList.add('dark-mode');
-      });
-      toggleTimelineColors();
-      chk.checked = true;
-    } else {
-      chk.checked = false;
-    }
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    document.body.classList.toggle("dark", isDarkMode);
+    header.classList.toggle("dark-mode", isDarkMode);
+    label.classList.toggle("dark-mode", isDarkMode);
+    navList.classList.toggle("dark-mode", isDarkMode);
+    toggleTimelineColors(isDarkMode);
+    chk.checked = isDarkMode;
 }
+
 checkDarkMode();
 
 chk.addEventListener("change", () => {
-  const isDarkMode = chk.checked;
-  localStorage.setItem("darkMode", isDarkMode);
-  document.body.classList.toggle("dark");
-  header.classList.toggle("dark-mode");
-  label.classList.toggle("dark-mode");
-  navList.classList.toggle("dark-mode");
-  toggleTimelineColors();
+    const isDarkMode = chk.checked;
+    localStorage.setItem("darkMode", isDarkMode);
+    checkDarkMode(); 
 });
 
-function toggleTimelineColors() {
-  const daysOfWeek = [
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ];
-  daysOfWeek.forEach((day) => {
-    const elements = document.querySelectorAll(`.timeline-${day}`);
-    elements.forEach((element) => {
-      element.classList.toggle("dark-mode");
+function toggleTimelineColors(isDarkMode) {
+    const daysOfWeek = [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+    ];
+    daysOfWeek.forEach((day) => {
+        const elements = document.querySelectorAll(`.timeline-${day}`);
+        elements.forEach((element) => {
+            element.classList.toggle("dark-mode", isDarkMode);
+        });
     });
-  });
 }
 
 function handleSvgHover(event) {
-
     const svgPath = event.currentTarget.querySelector('.home-icon path');
-
     const isHovered = svgPath.classList.contains('hovered');
-
     if (event.type === 'mouseover' && !isHovered) {
         svgPath.classList.add('hovered');
-    } 
-
-    else if (event.type === 'mouseout' && isHovered) {
+    } else if (event.type === 'mouseout' && isHovered) {
         svgPath.classList.remove('hovered');
     }
 }
